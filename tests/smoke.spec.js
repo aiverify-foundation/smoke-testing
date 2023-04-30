95b84557404bda4bb7af479292e24567c0a922b4
Benedict Lee <benedict_lee@imda.gov.sg>
2023-04-30 15:26:57 +0800

    Modified smoke.spec.js

--- tests/smoke.spec.js
+++ tests/smoke.spec.js
@@ -23,6 +23,7 @@ test('test', async () => {
   let randomNum = Math.random();
 
   const page = await context.newPage();
+  await page.setViewportSize({ width: 1920, height: 1080 });
   const projectName = 'Project ' + randomNum
   const projectDescription = 'Project 1'
   const reportTitle = 'Report for Project 1'
@@ -31,95 +32,113 @@ test('test', async () => {
   await page.goto('http://localhost:3000/home');
 
   // Add Dataset & Model
+  console.log('Add Dataset')
   await page.getByText('Models & Data').click();
   await page.getByText('Datasets').click();
   await page.getByRole('button', { name: 'New Dataset +' }).click();
   await page.getByText('Click to Browse').click();
-  await page.locator("input[name='file']").setInputFiles('/home/benflop/GitLab/end-to-end-testing/fixtures/pickle_pandas_tabular_loan_testing.sav');
+  await page.locator("input[name='file']").setInputFiles('/home/benflop/GitHub/smoke-testing/fixtures/pickle_pandas_tabular_loan_testing.sav');
   await page.getByRole('button', { name: 'Upload selected files >' }).click();
   await page.getByRole('button', { name: 'Back to all Datasets >' }).click();
+
+  console.log('Add Model')
   await page.locator('.MuiButtonBase-root').first().click();
   await page.getByTestId('open-model-list-button').locator('div').filter({ hasText: 'AI Models' }).locator('div').click();
   await page.getByRole('button', { name: 'New Model +' }).click();
   await page.getByRole('button', { name: 'upload' }).click();
   await page.getByRole('button', { name: 'Next >' }).click();
   await page.getByText('Click to Browse').click();
-  await page.locator("input[name='file']").setInputFiles('/home/benflop/GitLab/end-to-end-testing/fixtures/pickle_scikit_multiclasslr_loan.sav');
+  await page.locator("input[name='file']").setInputFiles('/home/benflop/GitHub/smoke-testing/fixtures/pickle_scikit_multiclasslr_loan.sav');
   await page.getByRole('button', { name: 'Upload selected files >' }).click();
   await page.getByRole('button', { name: 'Back to all Models >' }).click();
-  await page.getByText('pickle_scikit_multiclasslr_loan_1.sav').click();
-  await page.getByRole('button', { name: 'Edit' }).click();
-  await page.getByRole('button', { name: '​' }).click();
-  await page.getByRole('option', { name: 'Classification' }).click();
-  await page.getByRole('button', { name: 'Save & Close' }).click();
-  await page.getByTestId('HomeIcon').click();
+  await page.getByText('pickle_scikit_multiclasslr_loan.sav').click();
+  await page.getByTestId('MenuIcon').locator('path').click();
+  await page.getByText('Home').click();
 
   // Create a project
   console.log('Create A Project')
   await page.getByTestId('new-project-button').getByText('Test an AI Model and generate reports').click();
-  await page.getByPlaceholder('Enter Project Name').click();
-  await page.getByPlaceholder('Enter Project Name').fill(projectName);
-  await page.getByPlaceholder('Enter Project Description').click();
-  await page.getByPlaceholder('Enter Project Description').fill(projectDescription);
-  await page.getByTestId('input-report-title').click();
-  await page.getByPlaceholder('Enter Report Title').fill(reportTitle);
-  await page.getByPlaceholder('Enter Company Name').click();
-  await page.getByPlaceholder('Enter Company Name').fill(companyName);
-  await page.getByRole('button', { name: 'Next' }).click();
-  await page.getByText('Use a blank canvas').click();
+  await page.getByPlaceholder('Enter name of this project e.g. Credit Scoring Model Tests').fill(projectName);
+  await page.getByPlaceholder('Enter Project Description e.g. To test whether the classification model is fair towards all groups with respect to gender, robust against unexpected input and explainable.').click()
+  await page.getByPlaceholder('Enter Project Description e.g. To test whether the classification model is fair towards all groups with respect to gender, robust against unexpected input and explainable.').fill(projectDescription);
+  await page.getByPlaceholder('Enter the title to be used for the generated report').click();
+  await page.getByPlaceholder('Enter the title to be used for the generated report').fill(reportTitle);
+  await page.getByPlaceholder('Enter the company name').click();
+  await page.getByPlaceholder('Enter the company name').fill(companyName);
+  await page.getByText('Next').click();
+  await page.getByText('Blank CanvasDesign your own report by dragging widgets onto a blank canvas').click();
+  await page.getByText('Next').first().click();
 
   // Configure Global Variables
   console.log('Configure Global Variables')
-  await page.getByRole('button', { name: 'Configure Global Variables' }).click();
-  await page.getByRole('button', { name: 'Add Variable' }).click();
-  await page.getByRole('textbox').first().click();
-  await page.getByRole('textbox').first().fill('20');
-  await page.getByRole('textbox').first().press('Tab');
+  await page.getByRole('button', { name: 'Global Variables' }).click();
+  await page.getByRole('textbox').nth(1).click();
   await page.getByRole('textbox').nth(1).fill('20');
-  await page.getByRole('button', { name: 'OK' }).click();
-  await page.locator('.react-grid-layout').click();
+  await page.getByRole('textbox').nth(2).fill('20');
+  await page.locator('#globalVars button').click();
+
+  // Drag Widgets and Process Checklist
+  await page.getByRole('button', { name: 'AI Verify Process Checklist' }).click();
+  await page.locator('div:nth-child(2) > .leftpanel_widgetHeadingWrapper__ZJVYO > .leftpanel_draggableHandleIconWrapper__oeh3h').first().dragTo(page.locator('div.react-grid-layout'));
 
   // Add a page
-  // console.log('Add a page')
-  // await page.getByRole('button', { name: 'Add Page' }).click();
-  // await page.getByText('Next').click();
+  console.log('Add a page')
+  await page.locator('button:nth-child(3)').first().click();
 
-  // Drag FMT
-  console.log('Drag FMT')
-  await page.getByRole('button', { name: 'Widgets Add More' }).click();
-  await page.locator('div.droppable-element.MuiBox-root.mui-style-kpdj7g').filter({ hasText: 'Chart for False Discovery Rate metric' }).dragTo(page.locator('div.MuiPaper-root.MuiPaper-elevation.MuiPaper-rounded.MuiPaper-elevation3.mui-style-ye2xmv-MuiPaper-root'));
-  await page.locator('.react-grid-layout').click()
+  // Add Process Checklist
+  await page.getByRole('button', { name: 'Widgets for Fairness Metrics Toolbox' }).click();
+  await page.getByText('introduction to Fairness Metrics Toolbox for Classification').click();
+  await page.locator('div:nth-child(4) > .leftpanel_widgetHeadingWrapper__ZJVYO > .leftpanel_draggableHandleIconWrapper__oeh3h').first().dragTo(page.locator('div.react-grid-layout'));
+  await page.locator('.react-grid-layout').click();
   await page.getByText('Next').click();
 
   // Input Blocks & Technical Test
   console.log('Input Blocks & Technical Test')
   await page.getByRole('button', { name: 'Choose Dataset' }).first().click();
-  await page.getByText('pickle_pandas_tabular_loan_testing_1.sav').click();
+  await page.getByText('pickle_pandas_tabular_loan_testing.sav').click();
   await page.getByRole('button', { name: 'Use Dataset' }).click();
   await page.getByRole('button', { name: 'Choose Dataset' }).click();
-  await page.getByText('pickle_pandas_tabular_loan_testing_1.sav').click();
+  await page.getByText('pickle_pandas_tabular_loan_testing.sav').click();
   await page.getByRole('button', { name: 'Use Dataset' }).click();
   await page.getByRole('button', { name: '​' }).click();
   await page.getByRole('option', { name: 'Interest_Rate' }).click();
   await page.getByRole('button', { name: 'Choose Model' }).click();
-  await page.getByText('pickle_scikit_multiclasslr_loan_1.sav').click();
+  await page.getByText('pickle_scikit_multiclasslr_loan.sav').click();
   await page.getByRole('button', { name: 'Use Model' }).click();
   await page.getByRole('button', { name: 'Open' }).first().click();
   await page.getByRole('button', { name: 'Add Item' }).click();
   await page.getByLabel('sensitive_feature-0 *').click();
   await page.getByLabel('sensitive_feature-0 *').fill('Gender');
-  await page.getByRole('button', { name: 'Add Item' }).click();
   await page.getByLabel('sensitive_feature-1 *').click();
   await page.getByLabel('sensitive_feature-1 *').fill('Home_Owner');
   await page.getByRole('button', { name: 'Close' }).click();
-  await page.getByRole('button', { name: 'Generate Report' }).click();
+  await page.getByText('Next').click();
+
+  // Input Process Checklist
+  // await page.locator('[id="ibcard-aiverify\\.stock\\.process-checklist\\:fairness-process-checklist"]').getByRole('button', { name: 'Open' }).click();
+  // await page.getByTestId('completed-fairness-4').first().click();
+  // await page.getByTestId('completed-fairness-5').first().click();
+  // await page.getByTestId('completed-fairness-5').first().click();
+  // await page.getByTestId('completed-fairness-6').first().click();
+  // await page.getByTestId('completed-fairness-7').first().click();
+  // await page.getByRole('textbox').filter({ hasText: 'Test' }).click();
+  // await page.getByTestId('completed-fairness-8').first().click();
+  // await page.getByTestId('completed-fairness-9').first().click();
+  // await page.getByTestId('completed-fairness-10').first().click();
+  // await page.getByTestId('completed-fairness-10').first().click();
+  // await page.getByTestId('completed-fairness-11').first().click();
+  // await page.getByRole('button', { name: 'close' }).click();
+
+  // Start Repoet Generation
+  console.log('Report Generation')
   await page.getByRole('button', { name: 'Start Report Generation' }).click();
   await page.waitForURL('http://localhost:3000/reportStatus/*');
   await page.getByRole('button', { name: 'View Report' }).click();
-  const [download] = await Promise.all([
-    page.waitForEvent('download'),
-    page.getByRole('link', { name: 'Save' }).click()
-  ]);
+  await page.getByRole('img', { name: 'AI Verify' }).click();
+  // const [download] = await Promise.all([
+  //   page.waitForEvent('download'),
+  //   page.getByRole('link', { name: 'Save' }).click()
+  // ]);
 
   // Get Project Info directly from MongoDB
   console.log('Asserting Values')
@@ -132,6 +151,8 @@ test('test', async () => {
   expect(projectInfoObj[0].projectInfo.company).toBe(companyName)
   expect(projectInfoObj[0].pages).toBeNull
 
+  console.log(projectInfoObj[0].pages)
+
   // Get Project ID
   const projectId = projectInfoObj[0]._id
 
@@ -139,11 +160,9 @@ test('test', async () => {
   expect(projectInfoObj[0].globalVars[0].key).toBe('20')
   expect(projectInfoObj[0].globalVars[0].value).toBe('20')
 
-  await page.getByTestId('HomeIcon').locator('path').click();
-
   // View Report
-  console.log('View Report')
-  await page.getByText(projectName).click()
+  // console.log('View Report')
+  // await page.getByText(projectName).click()
   // await page.locator('div:nth-child(18) > .MuiPaper-root > div:nth-child(3) > button:nth-child(1)').click();
 
   // // Edit Project
