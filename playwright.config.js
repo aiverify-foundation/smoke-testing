8343087f3dbdeff668f5bf96d25f0ba9a83f86c7
Benedict Lee <benedict_lee@imda.gov.sg>
2023-11-18 11:13:17 +0800

    fix(playwright.config.js): Reduce Slow Mo to increase speed of Test Run

--- playwright.config.js
+++ playwright.config.js
@@ -54,7 +54,7 @@ const config = {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     launchOptions: {
-      slowMo: 300
+      slowMo: 100
     },
     video: 'on',
   },
