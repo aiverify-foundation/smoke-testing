a43c8f44eb0a6eefec6933dcba6e0f1ab3c7122d
Benedict Lee <118954493+imda-benedictlee@users.noreply.github.com>
2025-03-04 17:42:37 +0800

    Add Video Artefacts

--- playwright.config.js
+++ playwright.config.js
@@ -53,6 +53,7 @@ module.exports = defineConfig({
   
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
+    video: 'on',
     launchOptions: {
       slowMo: 1000
     },
